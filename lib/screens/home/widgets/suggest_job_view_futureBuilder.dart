84f7e0285f1627013d1f8c7b2105ae1166c51c23
mariam <mariamelmogy3@gmail.com>
2023-09-30 13:49:54 +0300

    - finish apply job

--- lib/screens/home/widgets/suggest_job_view_futureBuilder.dart
+++ lib/screens/home/widgets/suggest_job_view_futureBuilder.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:job_finder_app/screens/home/widgets/suggest_job.dart';
 import 'package:job_finder_app/services/job_api_service.dart';
 
+import '../../job_details/views/job_details_view.dart';
+
 class SuggestJobViewFutureBuilder extends StatelessWidget {
   const SuggestJobViewFutureBuilder({super.key});
 
@@ -12,7 +14,21 @@ class SuggestJobViewFutureBuilder extends StatelessWidget {
       builder: (context, snapshot) {
         if (!snapshot.hasError) {
           if (snapshot.connectionState == ConnectionState.done) {
-            return SuggestJob(job: snapshot.data!);
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return JobDetailsView(
+                        job: snapshot.data!,
+                      );
+                    },
+                  ),
+                );
+              },
+              child: SuggestJob(job: snapshot.data!),
+            );
           } else {
             return const Center(child: CircularProgressIndicator());
           }
